25ac56e58305dd310589c5944a7ff7b3ab76b024
JC <71771862+gls16@users.noreply.github.com>
2022-02-10 13:33:02 -0500

    Update Programa_Productos.cpp

--- ejecicios/Programa_Productos.cpp
+++ ejecicios/Programa_Productos.cpp
@@ -22,11 +22,6 @@ void comparar( Producto p, Producto q){
 		cout<<"El precio de los productos son iguales";
 }
 
-void descuento ( Producto &p){
-	p.pre = p.pre - (p.pre * 0.15);
-	cout<< p.nom <<" ahora vale: "<<p.pre<<endl;
-}
-
 string comparar_2(Producto p, Producto q){
 	if (p.pre > q.pre)
 		return p.nom;
@@ -35,25 +30,73 @@ string comparar_2(Producto p, Producto q){
 	else
 		cout<<"El precio de los productos son iguales";
 }
+
+void descuento ( Producto p){
+	p.pre = p.pre - (p.pre * 0.15);
+	cout<< p.nom <<" ahora vale con el descuento: "<<p.pre<<endl;
+}
+
+void iva ( Producto &p){
+	p.pre = p.pre + (p.pre * 0.19);
+	cout<< p.nom <<" ahora vale con el iva: "<<p.pre<<endl;
+}
+
+float iva_2 ( Producto &p){
+	p.pre = p.pre + (p.pre * 0.19);
+	return p.pre;
+}
+
+Producto comparador_menor( Producto &p, Producto &q){
+	if (p.pre > q.pre)
+		return q;
+	else if (p.pre < q.pre)
+		return p;
+	else
+		cout<<"El precio de los productos son iguales";
+}
+
+
+
 int main(){
 	
 	Producto pr1;
 	Producto pr2;
+	Producto aux;
+	Producto *v;
+	
+	v = &pr1;
 	
 	pr1.nom = "papa";
 	pr1.pre = 3000;
 	
-	mostrarProducto(pr1);
-	
 	pr2.nom = "Arroz";
 	pr2.pre = 5000;
 	
-	//comparar(pr1, pr2);	
+
+	mostrarProducto(pr1);
+	
+	comparar(pr1, pr2);	
 	
 	cout<<"el mas caro es "<<comparar_2(pr1, pr2)<<endl;
 	
 	descuento(pr1);
+	iva_2(pr1);
+	
 	descuento(pr2);
+	iva_2(pr2);
+	
 	mostrarProducto(pr1);
 	mostrarProducto(pr2);
+
+	aux = comparador_menor(pr1, pr2);
+	mostrarProducto(aux);
+	
+	cout<<v->nom<<endl;
+
+// [ -> ] : 1operador de acceso
+
+	Producto*v2=new Producto;
+	cout<<v2->pre;
+	
+	return 0;
 }
