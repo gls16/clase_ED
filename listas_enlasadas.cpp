7a39626b2b157dd416ee6648fb60f022965b768d
JC <71771862+gls16@users.noreply.github.com>
2022-02-15 13:36:36 -0500

    Update listas_enlasadas.cpp

--- listas_enlasadas.cpp
+++ listas_enlasadas.cpp
@@ -25,26 +25,46 @@ void InsertarInicio(Producto * &cab, string n, float p){
 	cab=nuevo;
 }
 
+void imprimir(Producto * cab){
+	Producto* aux=cab;
+	while(aux!=NULL){
+		cout<<"Prod "<<aux->nom<<" Valor "<<aux->pre<<endl;
+		aux=aux->sig;
+	}
+}
+
 int main(){
 	
 	Producto* cab =NULL;
 	string n;
 	float p;
 	
+	int op = 1;
+
 	cout<<"ingrese el noombre del producto"<<endl;cin>>n;
 	cout<<"ingrese el valor del producto"<<endl;cin>>p;
-	/*
+	
 	Crear(cab,n,p);
 	
+	/*
 	cout<<cab->nom<<endl;
 	cout<<cab->pre<<endl;
-	*/
+	
+	
+	cout<<cab->nom<<endl;
+	cout<<cab->pre<<endl;
 	
 	InsertarInicio(cab,n,p);
+	*/
 	
-	cout<<cab->nom<<endl;
-	cout<<cab->pre<<endl;
-	
-	
+		while( op == 1){
+		cout<<"ingrese el noombre del producto"<<endl;cin>>n;
+		cout<<"ingrese el valor del producto"<<endl;cin>>p;
+		InsertarInicio(cab,n,p);
+		cout<<"ingrese 1 para otro producto"<<endl;cin>>op;
+		}
+		
+		imprimir(cab);
+		
 	return 0;
 }
