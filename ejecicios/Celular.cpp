521fe88834d291f84751b6f8e0b516a721812376
JC <71771862+gls16@users.noreply.github.com>
2022-02-24 13:43:28 -0500

    Update Celular.cpp

--- ejecicios/Celular.cpp
+++ ejecicios/Celular.cpp
@@ -1,15 +1,18 @@
 # include<iostream>
 using namespace std;
+
 struct celular {
 	string marca;
 	int id;
 	celular* sig;
 };
 
-void insertar (celular*& cab, string marca, int id){
+void insertar (celular* &cab, string marca, int id){
+ 
  celular* nuevo= new celular; 
  nuevo->marca = marca;
  nuevo->id = id;
+ 
  if (cab == NULL ){
  	nuevo->sig = NULL;
  } else {
@@ -38,6 +41,36 @@ void insertarenelmedio (celular* cab, string marca, int id,int buscar){
 	 aux -> sig = nuevo;	
 }
 
+void eliminar(celular* &cab){
+	celular* aux = cab;
+	cab = cab->sig;
+	delete aux;
+}
+
+void eliminarElMedio(celular* cab, int b){
+	celular* aux = cab;
+	celular* ant = cab;
+	while (aux->sig){
+		aux = aux->sig;
+	}
+	while ( ant->sig!=aux){
+		ant=ant->sig;
+	}
+	ant->sig=aux->sig;
+	delete aux;
+}
+
+void eliminarElMedio_2(celular* cab, int b){
+	celular* aux = cab;
+	celular* ant = cab;
+	while (aux->id!=b){
+		aux = aux->sig;
+		ant=aux->sig;
+		
+	}
+	ant->sig=aux->sig;
+	delete aux;
+}
 
 int main (){
 	celular * cab = NULL;
@@ -52,17 +85,40 @@ int main (){
 	  insertar(cab, marca, id);
 	}
 	
+	cout<<endl<<"lista de celulares: "<<endl;
+		
 	imprimir (cab);
 	
+	/*
 	  cout << "Ingrese la marca del celular "<< endl;
 	  cin>> marca;
 	  cout << "Ingrese el id del celular "<< endl;
 	  cin >> id;
 	  
-	cout<<"Ingrese el celular a buscar: "<<endl;
+	
+	
+	cout<<"Ingrese el id del celular a buscar: "<<endl;
 	cin>>b;
 	
+	
+	
 	insertarenelmedio (cab, marca, id, b);
+	imprimir(cab);
+	
+	*/
+	
+	cout<<"\n\n\n";
+	
+	/*
+	eliminar(cab);
+	imprimir(cab);
+	*/
+	
+	cout<<"Ingrese el id del celular a eliminar: "<<endl;cin>>b;
+	
+	cout<<"\n\n\n";
+	
+	eliminarElMedio_2(cab,b);
 	imprimir(cab);
 	
 	return 0;
